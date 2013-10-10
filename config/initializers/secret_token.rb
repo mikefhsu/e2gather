bfa8c46ceb32d45d79df3e1bf0f0cfa0ecd7658a
vagrant <vagrant@precise32.(none)>
2013-10-10 20:07:28 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+E2gather::Application.config.secret_key_base = 'b26bf3b8f773e5ceb03fe68eab579ba02241248d35c13bfd29f5c965a9dac4301c6c38e27806a4517d56b9bf35882492ce6c6985cd5d1e043f912067bd376d8e'
