f4c6f54883665e93b8424bee733f78e2618c7039
Fang-hsiang Su <fs2455@columbia.edu>
2013-10-16 02:20:59 -0400

    Modify Gemfile and database.yml for mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20131015045755) do
 
   create_table "events", force: true do |t|
-    t.integer  "event_id"
-    t.string   "name"
-    t.string   "location"
-    t.datetime "date_time"
-    t.integer  "host"
+    t.integer  "event_id",        null: false
+    t.string   "name",            null: false
+    t.string   "location",        null: false
+    t.datetime "date_time",       null: false
+    t.integer  "host",            null: false
     t.text     "ingredient_list"
     t.text     "guest_list"
     t.boolean  "status"
@@ -27,20 +27,20 @@ ActiveRecord::Schema.define(version: 20131015045755) do
   end
 
   create_table "ingredients", force: true do |t|
-    t.integer  "ingredient_id"
-    t.string   "name"
-    t.float    "quantity"
-    t.string   "unit"
-    t.integer  "user_id"
+    t.integer  "ingredient_id", null: false
+    t.string   "name",          null: false
+    t.float    "quantity",      null: false
+    t.string   "unit",          null: false
+    t.integer  "user_id",       null: false
     t.boolean  "brought"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.string   "email"
+    t.integer  "user_id",           null: false
+    t.string   "name",              null: false
+    t.string   "email",             null: false
     t.text     "refrigerator_list"
     t.text     "event_list"
     t.datetime "created_at"
