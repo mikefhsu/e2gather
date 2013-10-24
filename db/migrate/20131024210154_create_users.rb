7ccf6c3072efdf3d8d3318e354ebbc7523862c63
vagrant <vagrant@precise32.(none)>
2013-10-24 22:14:05 +0000

    updating facebook login html

--- db/migrate/20131024210154_create_users.rb
+++ db/migrate/20131024210154_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do {:id => false} |t|
+    create_table :users do |t|
       t.string :user_id
       t.string :name
       t.string :email
-
       t.timestamps
     end
   end
