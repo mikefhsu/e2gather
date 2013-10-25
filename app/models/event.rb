4c845ec1386145a6ec41669b67c9f923b7735699
vagrant <vagrant@precise32.(none)>
2013-10-25 17:19:03 +0000

    updating models with has_many and belongs_to relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
-self.primary_key='event_id'
+  self.primary_key='event_id'
+  belongs_to :user
+  has_many :ingredients
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-self.primary_key='user_id'
+  self.primary_key='user_id'
+  has_many :ingredients, dependent: :destroy
+  has_many :events, dependent: :destroy
 end
