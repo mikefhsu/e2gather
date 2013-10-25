8bc36d2ebae97b04e97ebe7051f64ebe57e06857
Fang-hsiang Su <fs2455@columbia.edu>
2013-10-25 17:55:15 -0400

    First draft of User

--- app/controllers/e2gather_controller.rb
+++ app/controllers/e2gather_controller.rb
@@ -27,61 +27,56 @@ class E2gatherController < ApplicationController
 		  
     # auth established, now do a graph call:  
     @api = Koala::Facebook::API.new(session[:access_token])
-		begin
-			@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
-			user = @api.get_object("me")
+    begin
+      @graph_data = @api.get_object("/me/statuses", "fields"=>"message")
+      #@graph_data = @api.get_connections("me", "feed")
+      @email = @api.get_object("/me", "fields" => "email")
+      puts "Check email" + @email.to_s()
+      user = @api.get_object("me")
+      puts "Check user " + user.to_s()
 			
-			if User.where(user_id: user["id"]).exists?
-				@current_user = User.find(user["id"]);
-			else 
-				#@current_user = Users.new
-				@current_user=User.new(:user_id => user["id"], :name => user["name"],:email => user["email"])
-				@current_user.save
-			end 
+      if User.where(user_id: user["id"]).exists?
+        @current_user = User.find(user["id"]);
+      else
+	email = ""
+	if user["email"].nil?
+		email = user["username"] + "@facebook.com"
+	else
+		email =  user["email"]
+	end
+        @current_user=User.new(:user_id => user["id"], :name => user["name"],:email => email)
+        @current_user.save
+      end 
 			
-
-			
-			
-			@friends = @api.get_connections(user["id"], "friends")
-            @friend_list =getFriendList  
+    @friends = @api.get_connections(user["id"], "friends")
+    @friend_list =getFriendList  
 			 
-			#@friend_list.each do |f|
-				#puts f['id']
-			 #end
-		rescue Exception=>ex
-			puts ex.message
-		end
+    #@friend_list.each do |f|
+      #puts f['id']
+    #end
+    rescue Exception=>ex
+      puts ex.message
+    end
 		
   
- 		respond_to do |format|
-		 format.html {   }			 
-		end
-		
-   end
+    respond_to do |format|
+      format.html {   }			 
+    end		
+  end
 	
-		def sendInvitation
-	
-	
-	end
+  def sendInvitation
 	
 	
-	def getFriendList
-			friend_e2gather = []
-			@friends.each do |f|
-				if User.where(user_id: (f["id"])).exists?
-					#puts "find friend !"
-					friend_e2gather << f
-				end
-			end
-		return friend_e2gather
-		end
+  end
+	
+  def getFriendList
+    friend_e2gather = []
+    @friends.each do |f|
+      if User.where(user_id: (f["id"])).exists?
+        #puts "find friend !"
+        friend_e2gather << f
+      end
+    end
+    return friend_e2gather
+  end
 end
-
-
-
-
-
-
-
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,80 @@
 class User < ActiveRecord::Base
-  self.primary_key='user_id'
-  has_many :ingredients, dependent: :destroy
-  has_many :events, dependent: :destroy
+self.primary_key='user_id'
+has_many :ingredients, dependent: :destroy
+has_many :events, dependent: :destroy
+
+def get_name
+  return @name
+end
+
+def set_name(new_name)
+  @name = new_name 
+end
+
+def add_ingredient(new_ingredient)
+  if @ingredients.nil?
+    @ingredients = Array.new
+  end
+  @ingredients << new_ingredient
+end
+
+def delete_ingredient(ingred_id)
+  unless @ingredients.nil? or @ingredents.length == 0
+    @ingredients.each { |tmp|
+      if tmp.get_id() == ingred_id
+        @ingredients.delete(tmp)
+        return
+      end
+    }
+  end
+end
+
+def update_ingredent(ingred_id, quant)
+    unless @ingredients.nil? or @ingredients.length == 0
+      @ingredeints.each { |tmp|
+        if tmp.get_id() == ingred_id
+          diff = tmp.get_quant() - quant
+          if diff < 0
+            tmp.set_quant(0)
+          else
+            tmp.set_quant(diff)
+          end
+        end
+      }
+    end
+end
+
+def create_event(location, date, name)
+  if @events.nil?
+    @events = Array.new
+  end
+
+  Event e = Event.new
+  e.set_location(location)
+  e.set_date(date)
+  e.set_name(name)
+
+  e.save
+  @events << e
+end
+
+def update_event(event_id, acc_del)
+  #Combine accept_event and decline_event
+  #Need a method update_guest(user) in Event
+  unless @ingredients.nil? or @ @ingredients.length == 0
+    @ingredients.each { |tmp|
+      if tmp.get_id() == event_id
+        tmp.update_guest(self, acc_del)
+        return
+      end
+    }
+  end
+end
+
+def get_events
+  unless @ingredients.nil?
+    return @ingredients
+  end
+end
+
 end
