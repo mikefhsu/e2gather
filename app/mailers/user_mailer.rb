e2b2ca5fc98371360edde3431425ebe64269ebb5
vagrant <vagrant@precise32.(none)>
2013-10-31 03:02:14 +0000

    invite yourself

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -25,8 +25,9 @@ class UserMailer < ActionMailer::Base
   
   def invite_email(user, email, name, quantity, ingre_name ,event_name)
      mail(:to =>  email, :subject => 'Your friend '+user +' has just invited you to '+event_name, :body => "
-	 Hi, "+name +",please bring" +quantity +"unit(s) of "+ingre_name+"to "+event_name+", if you are interested! Please log into 
-	 e2gather to confirm or reject this event!")
+	 Hi, "+name +",please bring " +quantity +" unit(s) of "+ingre_name+" to "+event_name+", if you are interested in! Please log into 
+	 e2gather to confirm or reject this event!\n
+	 The E2Gather Team ")
   end
 
 end
