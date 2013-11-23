204865a55ac4508459bd5f212fd51545da7eefe8
Fang-hsiang Su <fs2455@columbia.edu>
2013-11-23 18:04:21 -0500

    Unit tests for invalid data length

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '4.0.0'
 gem 'koala', '1.3.0'
 gem 'bootstrap-sass', '2.3.2.0'
 
-# Use sqlite3 as the database for Active Record
+# Use sqlite3 as the testing database
 gem 'sqlite3'
 #gem 'tlsmail'
 # Use mysql as the database
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -6,16 +6,28 @@ class EventTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  @@event_name = "Test123"
+  @@event_host = "TestHost"
+  @@event_location = "Test location"
+  @@event_status = "Pending"
+  @@valid_date = DateTime.new(2014, 11, 24, 8, 52)
+  @@invalid_date = DateTime.new(2012, 11, 24, 8, 52)
+  @@within_bound = "wOElKAmO0dkOSGsxVif8IYZyRPdzAfJFVhMkuXjKuCza2mniJ7a7VelVIGBstd7QnGj0xQFzOGmBQgnGmQ25yg0HTAqaxZjZAyKP7GKNzkOQFHVqsh7pAnAGUNA3La1LboGDNmuto0SyJCtAYvVAFHqcf5g0bhasQddt4d5iQUc85UTkJCmoUgltrESxSKRZrhxiT3WMMNXdlPQ7RhKdSMI0wd98dUOOu09Jgyt83ZDls6iPLAurgZnbQqljyGm"
+  @@out_bound = "wOElKAmO0dkOSGsxVif8IYZyRPdzAfJFVhMkuXjKuCza2mniJ7a7VelVIGBstd7QnGj0xQFzOGmBQgnGmQ25yg0HTAqaxZjZAyKP7GKNzkOQFHVqsh7pAnAGUNA3La1LboGDNmuto0SyJCtAYvVAFHqcf5g0bhasQddt4d5iQUc85UTkJCmoUgltrESxSKRZrhxiT3WMMNXdlPQ7RhKdSMI0wd98dUOOu09Jgyt83ZDls6iPLAurgZnbQqljyGmI"
+
   test "should save event by complete information successfully" do
+	puts "Check within bound: " + @@within_bound.length.to_s
+	puts "Check out_bound: " + @@out_bound.length.to_s
+	
   	e = Event.new
-	e.name = "Test123"
-	e.host = "TestHost"
-	e.location = "Test location"
+	e.name = @@event_name
+	e.host = @@event_host
+	e.location = @@event_location
 	e.event_id = Time.now.to_i
-	e.status = "Pending"
+	e.status = @@event_status
 	
 	# Set date and time
-    	e.date_time = DateTime.new(2013, 11, 24, 8, 52)
+    	e.date_time = @@valid_date
 
     	# Collect ingredient and guest
     	ingredient_list =[]
@@ -36,13 +48,13 @@ class EventTest < ActiveSupport::TestCase
 
   test "should not save event by empty host" do
 	e = Event.new
-        e.name = "Test123"
-        e.location = "Test location"
+        e.name = @@event_name
+        e.location = @@event_location
         e.event_id = Time.now.to_i
-        e.status = "Pending"
+        e.status = @@event_status
 
         # Set date and time
-        e.date_time = DateTime.new(2013, 11, 24, 8, 52)
+        e.date_time = @@valid_date
 
         # Collect ingredient and guest
         ingredient_list =[]
@@ -63,9 +75,11 @@ class EventTest < ActiveSupport::TestCase
 
   test "should not save event by empty date" do
   	e = Event.new
-	e.name = "Test location"
+	e.name = @@event_name
+	e.host = @@event_host
+	e.location = @@event_location
 	e.event_id = Time.now.to_i
-	e.status = "Pending"
+	e.status = @@event_status
 	
 	# Collect ingredient and guest
         ingredient_list =[]
@@ -84,15 +98,170 @@ class EventTest < ActiveSupport::TestCase
         assert !e.save, "Save the event without a date"
   end
 
-  test "should not save two events by the same id" do
+  test "should not have events that have date is in the past" do
 	e = Event.new
-	e.name = "Test1"
-	e.event_id = 123
+	e.name = @@event_name
+	e.host = @@event_host
+	e.location = @@event_location
+	e.event_id = Time.now.to_i
+	e.status = @@event_status
 	
-	e2 = Event.new
-	e.name = "Test2"
-	e.event_id = 123
+	# Set date and time
+        e.date_time = @@invalid_date
 
-	assert !(e.save && e2.save), "Save the event with the same ids"
+	# Collect ingredient and guest
+        ingredient_list =[]
+
+        ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        # Add ingredient and guest to database
+        e.ingredient_list = ingredient_list.to_s
+        e.guest_list = ""
+        e.accept = 0
+        e.reject = 0
+        e.unconfirmed = ""
+
+	assert !e.save, "Save the event with the date that has been passed"
+  end
+
+  test "should have multiple events that has the same date, ingredients and guests without the same hosts" do
+  	#Create multiple events with the same contents except hosts
+	test_event_list = []
+	for i in 0...3
+		e = Event.new
+		e.name = @@event_name
+		e.host = @@event_host
+		e.location = @@event_location
+		e.event_id = 123
+		e.status = @@event_status
+		e.date_time = @@valid_date
+		
+		# Collect ingredient and guest
+        	ingredient_list =[]
+        	ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        	ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        	ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        	# Add ingredient and guest to database
+        	e.ingredient_list = ingredient_list.to_s
+        	e.guest_list = ""
+        	e.accept = 0
+        	e.reject = 0
+        	e.unconfirmed = ""
+  		
+		test_event_list[i] = e
+	end
+	
+	fake_id = 0
+	test_event_list.each {|tmp|
+		tmp.host = "TestHost"
+		fake_id += 1
+	}
+
+	assert test_event_list[0].save && test_event_list[1].save && test_event_list[2].save, "Save the events that has absolutely the same contents with different hosts"
+  end
+
+  test "should not have event with the length of name larger than 255" do
+  	e = Event.new
+	e.name = @@out_bound
+	e.host = @@event_host
+        e.location = @@event_location
+	e.event_id = Time.now.to_i
+	e.status = @@event_status
+        e.date_time = @@valid_date
+
+	# Collect ingredient and guest
+        ingredient_list =[]
+
+        ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        # Add ingredient and guest to database
+        e.ingredient_list = ingredient_list.to_s
+        e.guest_list = ""
+        e.accept = 0
+        e.reject = 0
+        e.unconfirmed = ""
+	
+	assert !e.save, "Save the event with name length larger than 255"
+
+  end
+
+  test "should have event with the length of name equal to 255" do
+  	e = Event.new
+	e.name = @@within_bound
+        e.location = @@event_location
+	e.event_id = Time.now.to_i
+	e.status = @@event_status
+	e.date_time = @@valid_date
+
+	# Collect ingredient and guest
+        ingredient_list =[]
+
+        ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        # Add ingredient and guest to database
+        e.ingredient_list = ingredient_list.to_s
+        e.guest_list = ""
+        e.accept = 0
+        e.reject = 0
+        e.unconfirmed = ""
+
+        assert e.save, "Save the event with name length eaual to 255"
+  end 
+
+  test "should not have event with the location with length larger than 255" do
+  	e = Event.new
+	e.name = @@event_name
+	e.location = @@out_bound
+	e.event_id = Time.now.to_i
+	e.status = @@event_status
+	e.date_time = @@valid_date
+	
+	# Collect ingredient and guest
+        ingredient_list =[]
+
+        ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        # Add ingredient and guest to database
+        e.ingredient_list = ingredient_list.to_s
+        e.guest_list = ""
+        e.accept = 0
+        e.reject = 0
+        e.unconfirmed = ""
+
+        assert !e.save, "Save the event with location length larger than 255"
+  end
+
+  test "should have event with the location with length equalt to 255" do
+        e = Event.new
+        e.name = @@event_name
+        e.location = @@within_bound
+        e.event_id = Time.now.to_i
+        e.status = @@event_status
+        e.date_time = @@valid_date
+
+        # Collect ingredient and guest
+        ingredient_list =[]
+
+        ingredient_list << Ingredient.new( :name=>  "ingred1", :ingredient_id =>0, :quantity=> 1,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred2", :ingredient_id =>0, :quantity=> 2,  :unit=>0, :user_id=> 0)
+        ingredient_list << Ingredient.new( :name=>  "ingred3", :ingredient_id =>0, :quantity=> 3,  :unit=>0, :user_id=> 0)
+
+        # Add ingredient and guest to database
+        e.ingredient_list = ingredient_list.to_s
+        e.guest_list = ""
+        e.accept = 0
+        e.reject = 0
+        e.unconfirmed = ""
+
+        assert e.save, "Save the event with location length eaual to 255"
   end
 end
